3c997c0b412d3be334df739cfda12faad1ce77eb
itt828 <iittt828@gmail.com>
2022-05-19 15:23:11 +0900

    :sparkles: add router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <div :class="$style.container">
+      <header :class="$style.header">
+        <router-link to="/">Home</router-link>
+      </header>
+
+      <router-view />
+    </div>
+  </main>
 </template>
 
-<style>
+<style module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
