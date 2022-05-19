28a0627f92ccfe958b335e3c8de97fee7bcf0aa0
itt828 <iittt828@gmail.com>
2022-05-19 15:49:10 +0900

    :sparkles: add axios page

---
+++ src/pages/Axios.vue
@@ -0,0 +1,23 @@
+<script setup>
+import axios from "axios";
+const sendGet = () => axios.get("https://ent5o104rfsmh.x.pipedream.net");
+const sendPost = () => axios.post("https://ent5o104rfsmh.x.pipedream.net", {
+  traQID: "temma",
+  grade: 19,
+});
+const sendPut = () => axios.put("https://ent5o104rfsmh.x.pipedream.net", {
+  testData: { string: "string", number: 1 },
+});
+const sendDelete = () => axios.delete("https://ent5o104rfsmh.x.pipedream.net", {
+  data: { po: 1 },
+});
+</script>
+
+<template>
+  <div>
+    <button @click="sendGet">GET</button>
+    <button @click="sendPost">POST</button>
+    <button @click="sendPut">PUT</button>
+    <button @click="sendDelete">DELETE</button>
+  </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/Home.vue";
+import Axios from "./pages/Axios.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const routes = [
   { path: "/", component: Home },
+  { path: "/axios", component: Axios },
   { path: "/:path(.*)", component: NotFound },
 ];
 
